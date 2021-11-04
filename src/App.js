a707215832ef0c6e7de37c461a6949179c963bad
Sordot <tgevirtz@gmail.com>
2021-11-03 21:01:25 -0500

    added currency abbreviations to the dropdown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Row from "./Row";
 
-
 function App() {
+
+  const [currencies, setCurrencies] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+
+  //fetch latest currency rates
+  useEffect(() => {
+
+    const fetchData = async () => {
+      //call the exchangerate api with base currency of USD
+      const apiResponse = await fetch(`https://api.exchangerate.host/latest?base=USD`)
+      const data = await apiResponse.json()
+      console.log(data)
+      
+      const starterCurrency = Object.keys(data.rates)[0]
+      //set our list of currencies for the dropdown as the abbreviations, starting with USD
+      setCurrencies([Object.keys(data.rates.USD), ...Object.keys(data.rates), ])
+      //set our starting currency to USD
+      setFromCurrency(data.base)
+      //set our default converted currency to the first item in our currencies array
+      setToCurrency(starterCurrency)
+    }
+    
+    fetchData();
+
+  },[])
+
+
   return <>
     <div className="App">
       <h1>Convert!</h1>
-      <Row />
+      <Row currencies={currencies}/>
       <div className='equals'>=</div>
-      <Row />
+      <Row currencies={currencies}/>
     </div>
   </>
 }
--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,13 @@
-const Row = () => {
+const Row = ({currencies}) => {
 
     return <>
         <div>
             <input type='number' className='input'/>
+            {/* map over each currency symbol and assign it an option in our select menu */}
             <select>
-                <option value='$$$'>$$$</option>
+                {currencies.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
             </select>
         </div>
     </>
