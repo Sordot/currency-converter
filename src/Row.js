800ea28d5aeccd857d581cbabdc1cd0e806a0a51
Sordot <tgevirtz@gmail.com>
2021-11-03 19:14:11 -0500

    initial templating and styles

---
+++ src/Row.js
@@ -0,0 +1,14 @@
+const Row = () => {
+
+    return <>
+        <div>
+            <input type='number' className='input'/>
+            <select>
+                <option value='$$$'>$$$</option>
+            </select>
+        </div>
+    </>
+
+}
+
+export default Row
